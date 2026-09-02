fa4bd4e094e133797f58f170a6b12aa16e3cb41c
Vaishnavi Goswami <vaishnavigoswamibsc24@rvu.edu.in>
2026-09-02 12:31:34 +0530

    Updated application

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to Cloud Platforms Lab</h1><h2>Python Flask Application Successfully Deployed!</h2>"
+    return "<h1>Cloud Platforms and Service Models</h1><h2>Updated Successfully!</h2>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
